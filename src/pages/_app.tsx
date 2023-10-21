bf16de92ab8b40bff11bf13a6de19f10b5df0700
kogajin <kogazin@gmail.com>
2023-10-22 03:12:04 +0900

    伝票追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,125 +24,7 @@ const App = () => {
     { label: "9", value: 9 },
     { label: "10", value: 10 },
   ];
-  const selectBox3 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
 
-  const selectBox4 = [
-    { label: "1", value: 1 },
-    { label: "2", value: 2 },
-    { label: "3", value: 3 },
-    { label: "4", value: 4 },
-    { label: "5", value: 5 },
-    { label: "6", value: 6 },
-    { label: "7", value: 7 },
-    { label: "8", value: 8 },
-    { label: "9", value: 9 },
-    { label: "10", value: 10 },
-  ];
-  const selectBox5 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
-
-  const selectBox6 = [
-    { label: "1", value: 1 },
-    { label: "2", value: 2 },
-    { label: "3", value: 3 },
-    { label: "4", value: 4 },
-    { label: "5", value: 5 },
-    { label: "6", value: 6 },
-    { label: "7", value: 7 },
-    { label: "8", value: 8 },
-    { label: "9", value: 9 },
-    { label: "10", value: 10 },
-  ];
-  const selectBox7 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
-
-  const selectBox8 = [
-    { label: "1", value: 1 },
-    { label: "2", value: 2 },
-    { label: "3", value: 3 },
-    { label: "4", value: 4 },
-    { label: "5", value: 5 },
-    { label: "6", value: 6 },
-    { label: "7", value: 7 },
-    { label: "8", value: 8 },
-    { label: "9", value: 9 },
-    { label: "10", value: 10 },
-  ];
-  const selectBox9 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
-
-  const selectBox10 = [
-    { label: "1", value: 1 },
-    { label: "2", value: 2 },
-    { label: "3", value: 3 },
-    { label: "4", value: 4 },
-    { label: "5", value: 5 },
-    { label: "6", value: 6 },
-    { label: "7", value: 7 },
-    { label: "8", value: 8 },
-    { label: "9", value: 9 },
-    { label: "10", value: 10 },
-  ];
-  const selectBox11 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
-
-  const selectBox12 = [
-    { label: "1", value: 1 },
-    { label: "2", value: 2 },
-    { label: "3", value: 3 },
-    { label: "4", value: 4 },
-    { label: "5", value: 5 },
-    { label: "6", value: 6 },
-    { label: "7", value: 7 },
-    { label: "8", value: 8 },
-    { label: "9", value: 9 },
-    { label: "10", value: 10 },
-  ];
-  const selectBox13 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
-  const selectBox14 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
-  const selectBox15 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
-  const selectBox16 = [
-    { label: "500", value: 500 },
-    { label: "600", value: 600 },
-    { label: "700", value: 700 },
-    { label: "800", value: 800 },
-  ];
   const [selectedValue1, setSelectedValue1] = useState<number>(0);
   const [selectedValue2, setSelectedValue2] = useState<number>(0);
 
@@ -212,26 +94,26 @@ const App = () => {
   };
   // 合計値
   const handleCalculateTotal3 = () => {
-    const result3 = selectedValue7 * selectedValue8;
-    setTotalValue2(result3);
+    const result3 = selectedValue5 * selectedValue6;
+    setTotalValue3(result3);
   };
-  // // ドリンク
-  // const handleSelectChange5 = (
-  //   event: React.ChangeEvent<{ value: unknown }>
-  // ) => {
-  //   setSelectedValue5(event.target.value as number);
-  // };
+  // ドリンク
+  const handleSelectChange7 = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
+    setSelectedValue7(event.target.value as number);
+  };
   // // 本数
-  // const handleSelectChange6 = (
-  //   event: React.ChangeEvent<{ value: unknown }>
-  // ) => {
-  //   setSelectedValue6(event.target.value as number);
-  // };
+  const handleSelectChange8 = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
+    setSelectedValue8(event.target.value as number);
+  };
   // // 合計値
-  // const handleCalculateTotal3 = () => {
-  //   const result4 = selectedValue5 * selectedValue6;
-  //   setTotalValue1(result4);
-  // };
+  const handleCalculateTotal4 = () => {
+    const result4 = selectedValue7 * selectedValue8;
+    setTotalValue4(result4);
+  };
   // // ドリンク
   // const handleSelectChange7 = (
   //   event: React.ChangeEvent<{ value: unknown }>
@@ -410,6 +292,48 @@ const App = () => {
         </div>
         <div>
           <p>選択値の合計: {totalValue3}</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <TextField
+            id="selectbox1"
+            label="初期設定のラベル1"
+            select
+            fullWidth
+            value={selectedValue7}
+            onChange={handleSelectChange7}
+          >
+            {selectBox1.map((item, index) => (
+              <MenuItem key={index} value={item.value}>
+                {item.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            id="selectbox2"
+            label="初期設定のラベル2"
+            select
+            fullWidth
+            value={selectedValue8}
+            onChange={handleSelectChange8}
+          >
+            {selectBox2.map((item, index) => (
+              <MenuItem key={index} value={item.value}>
+                {item.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleCalculateTotal4}
+          >
+            計算
+          </Button>
+        </div>
+        <div>
+          <p>選択値の合計: {totalValue4}</p>
         </div>
       </div>
       {/*
